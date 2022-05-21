b576df63b0e728d8d6f63c93d49f04e1327275bc
Paulo Cassola <98671508+PCassola@users.noreply.github.com>
2022-05-21 02:54:33 -0300

    Primeiro Commit

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel() // Configuração que diz qual é o servidor que implementa o modelo HTTP
+                .UseStartup<Startup>() // Aqui eu digo que toda a config de inicialização fica na classe startup
+                .Build(); 
+            host.Run();
+
+            // Imprime as Listas no Console
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
